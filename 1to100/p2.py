fc4decc5117e69aa100ab96ada0b80d0f2b6061d
Nathaniel Knapp <nathaniel.knapp@gmail.com>
2018-10-03 19:40:20 -0500

    problem 2

--- 1to100/p2.py
+++ 1to100/p2.py
@@ -1,2 +1,18 @@
 # By considering the terms in the Fibonacci sequence whose values do not exceed four million, find the sum of the even-valued terms.
 
+#1, 2, 3, 5, 8
+
+sm = 2
+previous_fib = 1
+fib = 2 
+
+while True:
+  if fib > 4*pow(10,6):
+    break
+  next_fib = fib + previous_fib
+  if next_fib%2 == 0:
+    sm += next_fib
+  previous_fib = fib
+  fib = next_fib
+
+print sm 
