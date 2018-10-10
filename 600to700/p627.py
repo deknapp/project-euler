f5df8706f466062315a2d1340e9feee492723668
Nathaniel Knapp <nathaniel.knapp@gmail.com>
2018-10-10 14:40:56 -0500

    version of problem 627 that works for F(9,2)

--- 600to700/p627.py
+++ 600to700/p627.py
@@ -1,39 +1,19 @@
-#Find F(30,10001) mod 1000000007.
+# unique products using n positive integers not exceeding m
 
-# Let's say it was only F(4, 10001)
-# Then, we can ignore 1 (1 to any power is 1
-# We get 10,001 combos from 3
-# We get 10,001 combos from 2
-# We do not get 10,001 combos from 4 because every multiple of 4 that is <= 2^10000 is a repeat
+primes_under_30 = [2,3,5,7,11,13,17,19,23,29]
+primes_under_9 = [2,3,5,7]
 
-def combos_from_prime(prime, max_multiples, max_integer, already_counted):
-  print 'prime is', prime
-  combos = max_multiples
-  number = 2*prime
-  while number <= 30:
-    if number in already_counted:
-      number += prime
-      continue
-    combos += max_multiples - max_multiples/number  
-    print sorted(list(set(already_counted)))
-    number += prime
-  return combos 
+def additional_products(primes, m, n):
+  counter = 0
+  for i in range(m):
+    counter += n - 1  
+  return counter
+  
+def F(m, n, primes_under_m):
+  counter = 0
+  for i in range(len(primes_under_m)):
+    counter += additional_products(primes_under_m[:i+1], m, n) 
+  return counter 
 
-m = 30
-n = 10001
-primes = [2,3,5,7,11,13,17,19,23,29]
-combos = [0]*len(primes) 
-already_counted = []
-for i in range(len(primes)):
-  prime = primes[i] 
-  temp = i 
-  while temp <= m:
-    already_counted.append(temp)
-    temp += prime  
-  combos[i] = combos_from_prime(prime, n, m, already_counted)
-
-total_combos = 1
-for combo in combos:
-  total_combos *= combo
- 
-print total_combos % (pow(10, 9) + 7)
+print F(30,2, primes_under_30)
+print F(9,2,primes_under_9)
